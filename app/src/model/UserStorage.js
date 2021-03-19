d9ac7ab037b16d6a729ff9083f46a23a8512f5cc
hjhwanggg <hhyejeong30@gmail.com>
2021-03-19 15:06:45 +0900

    User 모델 구현

---
+++ app/src/model/UserStorage.js
@@ -0,0 +1,32 @@
+
+class UserStorage {
+    static #users = {
+        id: ["test1", "나개발", "김팀장"],
+        psword: ["1234", "1234", "123456"],
+        name: ["테스트", "나개발", "김팀장"],
+    };
+
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users);    // => [id, psword, name]
+        const userInfo = userKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
+}
+
+module.exports = UserStorage
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,4 @@
-const users = {
-    id: ["test1", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
-};
+const User = require("../../model/User");
 
 const output = {
     home : (req, res)=>{
@@ -14,22 +11,9 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        const id = req.body.id,
-        psword = req.body.psword;
-
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                return res.json({
-                    success: true,
-                });
-            }
-        }
-
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하셨습니다.",
-        });
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response);
     },
 };
 
